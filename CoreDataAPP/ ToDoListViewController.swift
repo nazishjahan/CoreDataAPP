97b4d24a035b9541b44f41295f2c655380978147
NAZISH ZOHAIB <nazishzohaib@nazishs-macbook-pro.local>
2020-02-20 08:50:23 +0500

    Data persisted using UserDefault method

--- CoreDataAPP/ ToDoListViewController.swift
+++ CoreDataAPP/ ToDoListViewController.swift
@@ -9,35 +9,45 @@
 import UIKit
 
 class  ToDoListViewController: UITableViewController {
- let itemArray = ["buy Shoes","study hard","work day and night"]
+    let defaults = UserDefaults.standard
+   var itemArray = ["grab", "eat", "sleep"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     if let items = defaults.array(forKey: "ToDoListArray") as? [String]
+     { itemArray = items}
     }
-//  MARK - TableView Datasource Methods
-    
+   //MARK -TableView Datasource and Delegates
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return itemArray.count
+        return itemArray.count
     }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
-        return cell
+        let  cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")
+        cell?.textLabel?.text =  itemArray[indexPath.row]
+        return cell!
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-      
-       
-        
-        if   tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-             tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }else{
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            textField = alertTextField
+        }
+        alert.addAction( action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
--- CoreDataAPP/AppDelegate.swift
+++ CoreDataAPP/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask ,true))
         return true
     }
 
@@ -26,8 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("ApplicationDidEnterBackground")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -39,8 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
+        print("ApplicationWillTerminate")
         self.saveContext()
     }
 
